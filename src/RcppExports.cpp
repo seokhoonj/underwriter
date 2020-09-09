ddb9603cbd0913d1cac2c190d942ea1149b7e527
seokhoonj <seokhoonj@gmail.com>
2020-09-09 14:40:38 +0900

    file renamed

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,22 +5,22 @@
 
 using namespace Rcpp;
 
-// uniq_day
-NumericVector uniq_day(StringMatrix id, NumericVector from, NumericVector to);
-RcppExport SEXP _underwriter_uniq_day(SEXP idSEXP, SEXP fromSEXP, SEXP toSEXP) {
+// rcpp_uniq_day
+NumericVector rcpp_uniq_day(StringMatrix id, NumericVector from, NumericVector to);
+RcppExport SEXP _underwriter_rcpp_uniq_day(SEXP idSEXP, SEXP fromSEXP, SEXP toSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< StringMatrix >::type id(idSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type from(fromSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type to(toSEXP);
-    rcpp_result_gen = Rcpp::wrap(uniq_day(id, from, to));
+    rcpp_result_gen = Rcpp::wrap(rcpp_uniq_day(id, from, to));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_underwriter_uniq_day", (DL_FUNC) &_underwriter_uniq_day, 3},
+    {"_underwriter_rcpp_uniq_day", (DL_FUNC) &_underwriter_rcpp_uniq_day, 3},
     {NULL, NULL, 0}
 };
 
