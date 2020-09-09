0ad3e868540421341cdb24db321b66b58d64a1ea
seokhoonj <seokhoonj@gmail.com>
2020-09-09 14:39:45 +0900

    initial commit

---
+++ R/uniq_hos_day.R
@@ -0,0 +1,32 @@
+#' uniq_hos_day Function
+#'
+#' you can calculate the hospitalization days by several groups like id, icd code
+#' considering duplicated days.
+#' @param data.frame or data.table with hospitalization data like id, icd code, start date, end date.
+#' @keywords hospitalization
+#'
+uniq_hos_day <- function(data, var_id, var_from, var_to) {
+  # check arguments
+  if (missing(data)) stop('Please check input variables.')
+  if (class(data[[var_from]]) != 'Date') stop('Please check the var_from data type.')
+  if (class(data[[var_to]])   != 'Date') stop('Please check the var_to data type.')
+
+  # require data.table
+  require(data.table)
+  setDT(data)
+  setorderv(data, var_id)
+
+  # set arguments
+  id = as.matrix(data[, ..var_id])
+  from = data[[var_from]]
+  to = data[[var_to]]
+
+  # cpp code
+  hos <- rcpp_uniq_day(id, from ,to)
+
+  # binding with unique ids
+  data_uniq <- unique(data[, ..var_id])
+  z <- cbind(data_uniq, hos = hos)
+
+  return(z)
+}
