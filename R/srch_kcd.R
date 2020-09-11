f7b3fb19cd696e67d6223451a9df5213b13b342e
seokhoonj <seokhoonj@gmail.com>
2020-09-11 15:58:52 +0900

    missing and condition statements added

--- R/srch_kcd.R
+++ R/srch_kcd.R
@@ -4,19 +4,29 @@
 #' @param string A kcd code
 #' @examples
 #' srch_kcd('J00')
-srch_kcd = function(kcd, lang = 'kr') {
-  df <- kcd_book[grepl(kcd, code)]
-  nc <- max(nchar(df$code))
-  if (lang == 'kr') {
-    rc <- max(nchar(df$kr))
-    line <- nc + 2 + rc
-    result <- paste0(paste0(stri_pad_right(df$code, width = nc), ': ', df$kr), collapse = '\n')
+srch_kcd <- function (kcd, lang = "kr") {
+  if (missing(kcd))
+    stop('Please insert kcd code string or regular expression.')
+
+  if (any(grepl(kcd, kcd_book$code))) {
+    df <- kcd_book[grepl(kcd, code)]
+    nc <- max(nchar(df$code))
+    if (lang == "kr") {
+      rc <- max(nchar(df$kr))
+      line <- nc + 2 + ceiling(rc * 1.5)
+      result <- paste0(paste0(stri_pad_right(df$code, width = nc),
+                              ": ", df$kr), collapse = "\n")
+    }
+    else {
+      rc <- max(nchar(df$us))
+      line <- nc + 2 + rc
+      result <- paste0(paste0(stri_pad_right(df$code, width = nc),
+                              ": ", df$us), collapse = "\n")
+    }
+    cat(paste0(rep("=", times = line), collapse = ""), "\n")
+    cat(result, "\n")
+    cat(paste0(rep("=", times = line), collapse = ""), "\n")
   } else {
-    rc <- max(nchar(df$us))
-    line <- nc + 2 + rc * 2
-    result <- paste0(paste0(stri_pad_right(df$code, width = nc), ': ', df$us), collapse = '\n')
+    cat("No kcd code is found in the kcd book.")
   }
-  cat(paste0(rep('=', times = line), collapse=''), '\n')
-  cat(result, '\n')
-  cat(paste0(rep('=', times = line), collapse=''), '\n')
 }
