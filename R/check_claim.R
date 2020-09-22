5eaedff5be8f6ea638c651ccc22ef221f539cfc9
seokhoonj <seokhoonj@gmail.com>
2020-09-22 20:12:27 +0900

    initial commit

---
+++ R/check_claim.R
@@ -0,0 +1,11 @@
+#' Create claim boolean matrix
+#'
+#' you can create the first claim matrix considering insured id.
+#' @param code is a kcd code regular expression vector
+#' @param target is a kcd code vector
+#' @keywords claim boolean matrix
+check_claim <- function(code, target) {
+  vapply(code,
+         function(x) as.integer(grepl(x, target, perl = TRUE)),
+         FUN.VALUE = integer(length(target)))
+}
