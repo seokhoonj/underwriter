3eb67c4cc4eb5fcac028bec1f9d9752968e1b04b
seokhoonj <seokhoonj@gmail.com>
2020-09-22 20:36:47 +0900

    refreshed

--- R/check_first_claim.R
+++ R/check_first_claim.R
@@ -1,16 +1,16 @@
 #' Create first claim boolean matrix by insured id from claim boolean matrix
 #'
 #' you can create the first claim matrix considering insured id.
-#' @param id is a insured id numeric vector
 #' @param data is a boolean matrix (claim kcd code exists or not)
+#' @param id is a insured id numeric vector
 #' @keywords first claim
-check_first_claim <- function(id, data) {
+check_first_claim <- function(data, id) {
   if (!is.numeric(id))
     id <- as.numeric(id)
   if (!is.matrix(data))
     data <- as.matrix(data)
   m <- ncol(data)
   n <- nrow(data)
-  matrix(vapply(1:m, function(x) underwriter::rcpp_first_claim(id, data[, x]),
+  matrix(vapply(1:m, function(x) underwriter::rcpp_first_claim(data[, x], id),
                 FUN.VALUE = numeric(n)), ncol = m)
 }
