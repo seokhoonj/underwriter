425db2f7932477ee70f94d3b959dca06db80d023
seokhoonj <seokhoonj@gmail.com>
2020-09-23 14:32:53 +0900

    check_claim_kcd function added

---
+++ R/check_claim_kcd.R
@@ -0,0 +1,19 @@
+#' Create each rider's claim kcd code matrix
+#'
+#' you can create each rider's claim kcd code matrix (from the kcd code matrix).
+#' @param code is a kcd code regular expression vector
+#' @param target is a claim kcd code vector
+#' @param cores is a number of cores for parallel processing
+#' @keywords claim kcd code
+check_claim_kcd <- function(code, target, cores) {
+  if (missing(cores))
+    cores <- detectCores()
+  if (Sys.info()["sysname"] == "Linux") {
+    data <- rbindlist(list(mclapply(keyword, pull_claim_kcd_code_vec, target = target, mc.cores = cores)))
+  } else {
+    cl <- makeCluster(cores)
+    data <- rbindlist(list(parLapply(cluster, keyword, pull_claim_kcd_code_vec, target = target)))
+    stopCluster(cl)
+  }
+  return(data)
+}
