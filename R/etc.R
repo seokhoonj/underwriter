695a2cd6f77ebd8c1c17d6350a6ef51c249d30b5
seokhoonj <seokhoonj@gmail.com>
2020-09-23 14:55:08 +0900

    pull_claim_kcd function modified

--- R/etc.R
+++ R/etc.R
@@ -1,2 +1,2 @@
 glue_code <- function(code) paste0(code[!is.na(code)], collapse = '|')
-pull_claim_kcd <- function(code, target) unlist(lapply(regmatches(code, gregexpr(keyword, code, perl = TRUE)), paste0, collapse = '|'))
+pull_claim_kcd <- function(code, target) unlist(lapply(regmatches(target, gregexpr(code, target, perl = TRUE)), paste0, collapse = '|'))
