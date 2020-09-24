316c56116af71afde089e555a1f9390eaa7672a3
seokhoonj <seokhoonj@gmail.com>
2020-09-24 17:35:58 +0900

    function `splt_code` added

--- R/etc.R
+++ R/etc.R
@@ -1,2 +1,3 @@
 glue_code <- function(code) paste0(code[!is.na(code)], collapse = '|')
+splt_code  = function(code) strsplit(code, split = '\\|')[[1]]
 pull_claim_kcd <- function(code, target) unlist(lapply(regmatches(target, gregexpr(code, target, perl = TRUE)), paste0, collapse = '|'))
