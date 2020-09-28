eae3f629596a8a25bb4faa67a04760e0d11e1172
seokhoonj <seokhoonj@gmail.com>
2020-09-28 11:13:28 +0900

    add functions

--- R/etc.R
+++ R/etc.R
@@ -1,3 +1,5 @@
 glue_code <- function(code) paste0(code[!is.na(code)], collapse = '|')
-splt_code  = function(code) strsplit(code, split = '\\|')[[1]]
-pull_claim_kcd <- function(code, target) unlist(lapply(regmatches(target, gregexpr(code, target, perl = TRUE)), paste0, collapse = '|'))
+splt_code <- function(code) strsplit(code, split = '\\|')[[1]]
+excl_code <- function(code) paste0('^((?!', code, ').)*$')
+pull_code <- function(code, string) unique(unlist(regmatches(string, gregexpr(code, string))))
+pull_clm_kcd <- function(code, target) unlist(lapply(regmatches(target, gregexpr(code, target, perl = TRUE)), paste0, collapse = '|'))
