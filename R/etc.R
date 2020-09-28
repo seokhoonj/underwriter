aaa126c72d1df28a3862778cc3a5826b56d82910
seokhoonj <seokhoonj@gmail.com>
2020-09-28 13:34:26 +0900

    base functions added / stringr imported

--- R/etc.R
+++ R/etc.R
@@ -1,3 +1,12 @@
+
+# base functions ----------------------------------------------------------
+
+sizeof = function(x) format(object.size(x), unit = 'Mb')
+unilen = function(x) length(unique(x))
+getcol = function(df, str, contain = TRUE) if (contain) names(df)[ grepl(str, names(df))] else names(df)[!grepl(str, names(df))]
+
+# kcd code functions ------------------------------------------------------
+
 glue_code <- function(code) paste0(code[!is.na(code)], collapse = '|')
 splt_code <- function(code) strsplit(code, split = '\\|')[[1]]
 excl_code <- function(code) paste0('^((?!', code, ').)*$')
