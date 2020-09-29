61d8c2f4080362f207219e9e0415a8047a09529c
seokhoonj <seokhoonj@gmail.com>
2020-09-29 15:35:09 +0900

    cut_age function added

---
+++ R/cut_age.R
@@ -0,0 +1,14 @@
+#' Categorize the age variable
+#'
+#' you can categorize the age variable.
+#' @param data is a claim data
+#' @param var is a age column variables
+#' @param interval is an interval number
+#' @param right is a limit direction (default FALSE)
+#' @keywords hospitalization
+cut_age <- function(data, var, interval, right = FALSE) {
+  age <- eval(substitute(var), data)
+  min <- floor(min(age) / interval) * interval
+  max <- ceiling(max(age) / interval) * interval
+  cut(age, breaks = seq(min, max, interval), right = right)
+}
