eae4923b2bccebe181812549bf0ac4f613ce13a4
seokhoonj <seokhoonj@gmail.com>
2020-09-29 17:26:16 +0900

    clm_sim_sur_grd function added

---
+++ R/clm_sim_sur_grd.R
@@ -0,0 +1,22 @@
+#' Create surgery grade simulation matrix
+#'
+#' you can create the surgery grade simulation matrix from surgery simulation matrix
+#' @param info is a claim book with surgery grade rider information.
+#' @param data is a surgery simulation matrix (through the clm_sim function)
+#' @param target is a surgery grade vector
+#' @keywords surgery grade
+clm_sim_sur_grd <- function(info, clm_sim, target) {
+  col <- target
+  row <- seq_along(target)
+  rng <- unname(table(info$rider))
+  loc <- c(0, cumsum(rng))
+
+  z = list()
+  for (j in seq_along(rng)) {
+    datum <- data[, (loc[j]+1):(loc[j+1])]
+    vec <- vector(length = length(target))
+    for (i in row) vec[i] <- datum[row[i], col[i]]
+    z[[j]] <- vec
+  }
+  matrix(unlist(z), ncol = length(rng))
+}
