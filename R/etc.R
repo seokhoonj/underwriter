29324483565737059dd0e275a85d75b984c4479a
seokhoonj <seokhoonj@gmail.com>
2020-10-04 20:23:53 +0900

    diff_mon function added

--- R/etc.R
+++ R/etc.R
@@ -14,3 +14,8 @@ splt_code <- function(code) strsplit(code, split = '\\|')[[1]]
 excl_code <- function(code) paste0('^((?!', code, ').)*$')
 pull_code <- function(code, string) unique(unlist(regmatches(string, gregexpr(code, string))))
 pull_clm_kcd <- function(code, target) unlist(lapply(regmatches(target, gregexpr(code, target, perl = TRUE)), paste0, collapse = '|'))
+
+
+# date functions ----------------------------------------------------------
+
+diff_mon <- function(date, origin) (year(date) - year(as.Date(origin))) * 12 + month(date) - month(as.Date(origin)) + 1L
