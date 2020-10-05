05e58d4e9da5004ae49bef54b0751ce369e62a2a
seokhoonj <seokhoonj@gmail.com>
2020-10-05 16:32:19 +0900

    chk_prm_cnt function added

---
+++ R/chk_prm_cnt.R
@@ -0,0 +1,52 @@
+#' Create number of premium payment data by id
+#'
+#' you can create number of premium payment from virtual underwritng date
+#' @param info is a claim information file
+#' @param data is a claim history file
+#' @param origin is an origin date
+#' @param mon is a payment duration
+#' @keywords number of premium payment
+chk_prm_cnt <- function(info, data, origin, mon) {
+  # set info variables
+  rdr <- info$rdr_kr
+  code <- info$kcd
+  otime <- info$otime
+
+  # set data variables
+  id <- data$id
+  cdate <- data$sdate
+  target <- data$kcd
+
+  # m, n
+  m <- nrow(info)
+  n <- nrow(data)
+
+  # clm_cmb & clm_mon
+  clm <- chk_clm(code, target)
+  clm_1st <- chk_clm_1st(clm, id)
+  clm_cmb <- cmb_clm_1st(clm, clm_1st, otime)
+  clm_mon <- chk_clm_mon(cdate, origin, m)
+
+  # clm_mon
+  clm_mon[clm_cmb == 0] <- 0L
+  rownames(clm_mon) <- id
+  tmp <- as.data.table(clm_mon, keep.rownames = 'id')[, lapply(.SD, max), by = .(id)]
+  names(tmp)[-1] <- rdr
+
+  # set column variables
+  col_ot <- c('id', names(tmp[,-1])[which(otime == 1)])
+  col_re <- c('id', names(tmp[,-1])[which(otime == 0)])
+
+  # transform onetime matrix
+  mat_ot <- as.matrix(tmp[, ..col_ot][,-1])
+  mat_ot[mat_ot == 0] <- mon
+
+  # transform repeat matrix
+  mat_re <- as.matrix(tmp[, ..col_re][,-1])
+  mat_re[] <- mon
+
+  # combine
+  df <- data.table(id = tmp$id, mat_ot, mat_re)
+  cl <- c('id', unique(rdr))
+  df[, ..cl]
+}
