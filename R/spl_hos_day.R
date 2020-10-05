fba70a6604f3ce8564bae2cd3d250dbea811b0df
seokhoonj <seokhoonj@gmail.com>
2020-10-05 21:32:06 +0900

    comma subtracted

--- R/spl_hos_day.R
+++ R/spl_hos_day.R
@@ -8,10 +8,10 @@
 spl_hos_day <- function(data, var_from, var_to, origin, all = TRUE) {
   from  <- eval(substitute(var_from), data)
   to    <- eval(substitute(var_to), data)
-  tmp_e <- data[from >= as.Date(origin) | to <= as.Date(origin),]
-  tmp_b <- data[from < as.Date(origin) & to > as.Date(origin),]
+  tmp_e <- data[from >= as.Date(origin) | to <= as.Date(origin)]
+  tmp_b <- data[from < as.Date(origin) & to > as.Date(origin)]
   tmp_a <- copy(tmp_b)
-  tmp_b[[deparse(substitute(var_to))]] <- as.Date(origin) - 1L
+  tmp_b[[deparse(substitute(var_to))]] <- as.Date(origin)-1L
   tmp_a[[deparse(substitute(var_from))]] <- as.Date(origin)
   if (all) return(rbind(tmp_e, tmp_b, tmp_a)) else return(rbind(tmp_b, tmp_a))
   cat('please check hospitalization days or claim year, you may have to recalculate!')
