29da9eacfbb05b9a7fd2b100e4811d6343ba7208
seokhoonj <seokhoonj@gmail.com>
2020-10-07 17:27:18 +0900

    grd variable added

--- R/prm_sim.R
+++ R/prm_sim.R
@@ -10,14 +10,15 @@
 prm_sim <- function(rsk_info, clm_info, data, origin, yrs) {
   rdr <- unique(clm_info$rdr_kr) # rider
   prf <- unique(data[, .(id, age, gnd)]) # profile
-  pop <- prf[, .(.N), .(age, gnd)][order(gnd, age)] # population
+  pop <- prf[, .(.N), .(age, gnd, grd)][order(age, gnd, grd)] # population
   npm <- cbind(prf, chk_prm_cnt(clm_info, data, origin, yrs)) # number of payment
 
   prm_vec <- as.list(vector(length = nrow(pop)))
   for (j in 1:nrow(pop)) {
     age_var <- pop$age[j]
     gnd_var <- pop$gnd[j]
-    prm <- chk_prm(rsk_info, rdr, age_var, gnd_var, 2, yrs)
+    grd_var <- pop$grd[j]
+    prm <- chk_prm(rsk_info, clm_info, age_var, gnd_var, grd_var, yrs)
     tmp <- npm[age == age_var & gnd == gnd_var]
     id <- tmp$id
     tmp <- as.matrix(tmp[, -c(1:3)])
