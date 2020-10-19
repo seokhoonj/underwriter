d2ade324fd35945c0a08d1900e6be31c0477a6ed
seokhoonj <seokhoonj@gmail.com>
2020-10-19 15:49:11 +0900

    elapsed_days function added

---
+++ R/elapsed_day.R
@@ -0,0 +1,20 @@
+#' Calculate elapsed days
+#'
+#' you can calculate the minimum elapsed days by several groups like id, icd code
+#' considering duplicated days.
+#' @param ... claim data with id, icd code, start date, end date.
+#' @param var_id grouping column variables
+#' @param var_to end date column variable
+#' @param origin is an origin date
+#' @keywords elapsed days
+elapsed_day <- function(..., var_id, var_to, origin) {
+  l <- list(...)
+  var_id <- vapply(substitute(var_id)[-1L], deparse, FUN.VALUE = "character")
+  var_to <- deparse(substitute(var_to))
+  v <- as.list(vector(length = length(l)))
+  for (i in seq_along(l)) {
+    v[[i]] <- l[[i]][, .(elapsed = vapply(.SD, function(x) as.numeric(as.Date(origin) - max(x)), FUN.VALUE = 1)), var_id, .SDcols = var_to]
+  }
+  z <- do.call('rbind', v)
+  z[, .(elapsed = vapply(.SD, min, FUN.VALUE = 1)), var_id, .SDcols = 'elapsed']
+}
