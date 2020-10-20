ec36b13ae1b1b1ad03beb3b73f26fe30facb4d43
seokhoonj <seokhoonj@gmail.com>
2020-10-20 20:41:35 +0900

    eval(var_to) added

--- R/elapsed_day.R
+++ R/elapsed_day.R
@@ -2,21 +2,14 @@
 #'
 #' you can calculate the minimum elapsed days by several groups like id, icd code
 #' considering duplicated days.
-#' @param ... claim data with id, icd code, start date, end date.
-#' @param var_id grouping column variables
-#' @param var_to end date column variable
+#' @param data is claim data with id, icd code, start date, end date.
+#' @param var_id is the grouping column variables
+#' @param var_to is the end date column variable
 #' @param origin is an origin date
 #' @keywords elapsed days
-elapsed_day <- function(..., var_id, var_to, origin) {
-  l <- list(...)
+elapsed_day <- function(data, var_id, var_to, origin) {
   var_id <- vapply(substitute(var_id), deparse, FUN.VALUE = "character")
-  var_id <- names(l[[1L]])[match(var_id, names(l[[1L]]), 0L)]
-  var_to <- deparse(substitute(var_to))
-  v <- as.list(vector(length = length(l)))
-  for (i in seq_along(l)) {
-    v[[i]] <- l[[i]][, .(elapsed = vapply(.SD, function(x) as.numeric(as.Date(origin) - max(x)), FUN.VALUE = 1)), var_id, .SDcols = var_to]
-  }
-  z <- do.call('rbind', v)
-  setorderv(z, var_id)
-  z[, .(elapsed = vapply(.SD, min, FUN.VALUE = 1)), var_id, .SDcols = 'elapsed']
+  var_id <- names(data)[match(var_id, names(data), 0L)]
+  var_to <- parse(text = deparse(substitute(var_to)))
+  data[, .(elapsed = as.numeric(as.Date(origin) - max(eval(var_to)))), var_id]
 }
