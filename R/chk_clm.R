0ba0274e3d954a772d1764574455559acee6b505
seokhoonj <seokhoonj@gmail.com>
2020-10-22 17:33:43 +0900

    return type changed to matrix

--- R/chk_clm.R
+++ R/chk_clm.R
@@ -5,7 +5,7 @@
 #' @param target is a kcd code vector
 #' @keywords claim boolean matrix
 chk_clm <- function(code, target) {
-  vapply(code,
-         function(x) as.double(grepl(x, target, perl = TRUE)),
-         FUN.VALUE = double(length(target)), USE.NAMES = FALSE)
+  matrix(vapply(code,
+                function(x) as.double(grepl(x, target, perl = TRUE)),
+                FUN.VALUE = double(length(target)), USE.NAMES = FALSE), ncol = length(code))
 }
