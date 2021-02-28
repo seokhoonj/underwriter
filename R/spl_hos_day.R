8ee627b2671811ef294794a3250b324d758deab6
seokhoonj <seokhoonj@gmail.com>
2021-02-28 14:26:02 +0900

    spl_hos_day function is modified

--- R/spl_hos_day.R
+++ R/spl_hos_day.R
@@ -5,14 +5,31 @@
 #' @param var_from start date column variable
 #' @param var_to end date column variable
 #' @keywords split hospitalization days by origin date
-spl_hos_day <- function(data, var_from, var_to, origin, all = TRUE) {
-  from  <- eval(substitute(var_from), data)
-  to    <- eval(substitute(var_to), data)
-  tmp_e <- data[from >= as.Date(origin) | to <= as.Date(origin)]
-  tmp_b <- data[from < as.Date(origin) & to > as.Date(origin)]
-  tmp_a <- copy(tmp_b)
-  tmp_b[[deparse(substitute(var_to))]] <- as.Date(origin)-1L
-  tmp_a[[deparse(substitute(var_from))]] <- as.Date(origin)
-  if (all) return(rbind(tmp_e, tmp_b, tmp_a)) else return(rbind(tmp_b, tmp_a))
-  cat('please check hospitalization days or claim year, you may have to recalculate!')
+spl_hos_day <- function(data, var_from, var_to) {
+  str_from <- deparse(substitute(var_from))
+  str_to   <- deparse(substitute(var_to))
+
+  s <- as.integer(substr(data[[str_from]], 1, 4))
+  e <- as.integer(substr(data[[str_to]]  , 1, 4))
+  years <- unique(c(s[which(e-s > 0)], e[which(e-s > 0)]))
+  dates <- as.Date(paste0(as.character(years), "0101"), "%Y%m%d")
+
+  for (i in seq_along(dates)) {
+    from <- eval(substitute(var_from), data)
+    to   <- eval(substitute(var_to), data)
+
+    tmp_e <- data[from >= dates[i] | to <  dates[i]]
+    tmp_b <- data[from <  dates[i] & to >= dates[i]]
+    tmp_a <- copy(tmp_b)
+
+    if (nrow(tmp_a) > 0) {
+      tmp_b[[str_to]]   <- dates[i]-1L
+      tmp_a[[str_from]] <- dates[i]
+    }
+
+    data <- rbind(tmp_e, tmp_b, tmp_a)
+  }
+  cat("Please check hospitalization days or claim year, you may have to recalculate!\n")
+  setorderv(data, names(data))
+  return(data)
 }
